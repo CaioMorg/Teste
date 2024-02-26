c8429b063b27b375837e65f1d579ac9bee0050dd
CaiorMorg <morgoncaio1997@outlook.com>
2024-02-26 14:29:45 -0300

    colocar barra de navegação

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 
 let trilho = document.getElementById('trilho')
 let body = document.querySelector('body')
+let ul = document.querySelector('ul.nav')
 trilho.addEventListener('click', ()=>{
     trilho.classList.toggle('dark')
     body.classList.toggle('dark')
+    ul.classList.toggle('dark')
 
 })
